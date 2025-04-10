6af08b69186d43b0b84349fdf419d984dea817c6
zwlucas <lucas.oliveira1676@etec.sp.gov.br>
2025-04-09 22:57:32 -0300

    test

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -10,11 +10,10 @@ export default async function AuthButton() {
 
     const { data: { user }, } = await supabase.auth.getUser();
     const session = await supabase.auth.getSession();
-    console.log(session)
 
     return user ? (
         <div className="flex items-center gap-4">
-            Olá, {user.user_metadata?.name || user.user_metadata?.user_name || user.email}
+            Olá, {session.data.session?.access_token} {/* {user.user_metadata?.name || user.user_metadata?.user_name || user.email} */}
 
             <form action={signOutAction}>
                 <Button type="submit" variant="outline">
